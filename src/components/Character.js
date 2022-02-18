8ef43f8a01e2a9a08a64e92b357ea0b0d39314c1
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-02-17 22:49:49 -0700

    Pulled API data and added a few styles

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,38 @@
-// Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+
+const StyledName = styled.h2 `
+    &: hover {
+        color: white;
+        text-shadow: none;
+    }
+`
+
+const StyledP = styled.p `
+    &: hover {
+        color: black;
+        text-shadow: none;
+    }
+`
+
+
+
+const Character = (props) => {
+    return (
+        <>
+            {props.character.map(char => {
+                return (
+                    <div>
+                        <StyledName>Name: {char.name}</StyledName>
+                        <StyledP>Birth Year: {char.birth_year}</StyledP>
+                        <StyledP>Height: {char.height}</StyledP>
+                        <StyledP>Mass: {char.mass}</StyledP>
+                    </div>
+                )
+            })}
+        </>
+    )
+}
+
+export default Character;
